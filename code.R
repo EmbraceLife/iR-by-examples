82483de98b975a2710e0c04ad7ddf5ad615859f7
EmbraceLife <1227561934@qq.com>
2016-08-06 09:58:45 +0800

    add

--- code.R
+++ code.R
@@ -1,44 +1,10 @@
-# iR-by-examples
-Learning R by examples
-
-## Basics
-
-get and set working directory
-```r
 getwd()
 setwd("/Users/Natsume/Documents/1learnD3/d3-template/")
-```
-
-check files inside a folder
-```r
 dir()
-```
-
-read csv files
-```r
 hkws <- read.csv("hkws.csv")
-
-?read.csv # check use of different parameters
-
+?read.csv # check use of different parameters 
 hkws <- read.csv("hkws.csv", header=F)
-
-hkws %>% head()
-```
-
-
-Load libraries
-```r
 library(dplyr)
-```
-
-
-add or change column names
-```r
+hkws %>% head()
 colnames(hkws) <- c("Date", "Open", "High", "Low", "Close", "Volume", "Cash")
-```
-
-
-write a csv file
-```r
 write.csv(hkws, file = "/Users/Natsume/Documents/1learnD3/d3-template/hkwsName.csv")
-```
